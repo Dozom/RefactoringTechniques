2351bf6677ec5fbc5066a21e1aace8d9e7a41123
dozom <danielnagercarpios@gmail.com>
2023-06-30 16:08:00 +0200

    Adding the refactored version with split Phase refactoring

--- splitPhase/splitPhaseRefactored.js
+++ splitPhase/splitPhaseRefactored.js
@@ -18,19 +18,22 @@
 */
 
 function priceOrder(product, quantity, shippingMethod) {
-    const basePrice = product.basePrice * quantity;
-    const discount = Math.max(quantity - product.discountThreshold, 0)
-        * product.basePrice * product.discountRate;
-    const price = applyShipping(basePrice, shippingMethod, quantity, discount);
-    return price;
+    const priceData = calculatePricingData(product, quantity);
+    return applyShipping(priceData, shippingMethod, quantity);
 }
 
-function applyShipping(basePrice, shippingMethod, quantity, discount){
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+function calculatePricingData(product, quantity){
+    const basePrice = product.basePrice * quantity;
+    const discount = Math.max(quantity - product.discountThreshold, 0)
+       * product.basePrice * product.discountRate;
+    return {basePrice: basePrice, quantity: quantity, discount: discount};
+}
+
+function applyShipping(priceData, shippingMethod){
+    const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
-    const shippingCost = quantity * shippingPerCase;
-    const price = basePrice - discount + shippingCost;
-    return price;
+    const shippingCost = priceData.quantity * shippingPerCase;
+    return priceData.basePrice - priceData.discount + shippingCost;
 }
 
 module.exports = priceOrder;
